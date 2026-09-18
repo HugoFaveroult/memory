32a2328e06418b9781cfaec6751ae5e9171f2c09
Hugo Faveroult <faverou241@unicaen.fr>
2026-09-18 09:52:19 +0200

    feat: core array data logic and fisher-yates shuffle functional

---
+++ JS/script.js
@@ -0,0 +1,33 @@
+let dimension = 150;
+let imgStart = Math.floor(Math.random() * 100) + 1;
+
+const images = [];
+for(let i = imgStart; i < imgStart + 7; i++){
+    images.push(`https://picsum.photos/seed/${i}/${dimension}/${dimension}`);
+}
+let cards =[...images, ...images];
+
+console.table(cards);
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
